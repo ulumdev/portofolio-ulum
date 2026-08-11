4b9da7cf07d950ca443ab8a619ec52ed083aac41
ulumdev <belajarkoding97@gmail.com>
2026-08-12 01:11:51 +0700

    Configure trustProxies to fix HTTPS mixed content

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies (Nginx Reverse Proxy)
+        $middleware->trustProxies(at: '*');
+        
         // Register middleware alias
         $middleware->alias([
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
